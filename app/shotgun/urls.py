77b250e7581997ad9e31f79c197e9429d10b6958
Jesse <jesseschloess@gmail.com>
2025-07-31 14:25:20 -0700

    working index site

--- app/shotgun/urls.py
+++ app/shotgun/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("bets/", include("bets.urls")),
+    path("", include("bets.urls")),
     path('admin/', admin.site.urls),
 ]
